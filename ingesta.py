09ab4fe9361301d736786c4afc4cd38d2b630d94
FernandoGuillenR <66321787+FernandoGuillenR@users.noreply.github.com>
2026-05-10 21:00:12 -0500

    Integrate MySQL data extraction and S3 upload
    Extract data from MySQL and upload to S3.

--- ingesta.py
+++ ingesta.py
@@ -1,4 +1,19 @@
 import boto3
+import pymysql
+import pandas as pd
+
+# ---CONFIGURACIÓN MYSQL ---#
+db_host = "db"
+db_user = "root"
+db_pass = "academy_pass"
+db_name = "laboratorio"
+
+print("Extrayendo datos de MySQL...")
+conexion = pymysql.connect(host=db_host, user=db_user, password=db_pass, database=db_name)
+
+df = pd.read_sql("SELECT * FROM usuarios", conexion)
+df.to_csv("data.csv", index=False)
+conexion.close()
 
 ficheroUpload = "data.csv"
 nombreBucket = "gcr-output-01"
